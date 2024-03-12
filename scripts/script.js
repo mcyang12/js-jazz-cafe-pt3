eeb473248713380b4b8e58b17e51d158d751f847
Mase <mase@hackthegap.com>
2024-03-12 17:36:40 +0000

    Added instructions

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+// Task 1: Implement a function that calculates total coffees sold
+    // Calculate the total coffees sold by summing up a sales array
+
+
+
+// Task 2: Implement a function to filter out decaf coffees from a list of coffee options
+    // Use the filter method to filter out decaf coffee options
+
+
+
+// Task 3: Define a class for Jazz Coffee Roasters
+
+
+// Task 4: Extend the Jazz Coffee Roaster class for Specialty Coffee Roasters
+
+
+// Task 5: Create a custom constructor function for Jazz Coffee Blends
+
+
+// Task 6: Implement a class with default parameters for Jazz Coffee Cups
+
+
+// Task 7: Use template literals to create a message about a coffee event
+    //The event details have been given to you below
+    const coffeeEvent = {
+        name: "Java Jam",
+        date: "April 10, 2024",
+        venue: "Jazz & Java"
+    };
+    
+    
+    
+    // Task 9: Use spreadrest operators on an array
+    
+    // Array of coffee types
+    const coffeeTypes = ["Espresso", "Latte", "Cappuccino"];
+    const newType = "Mocha";
+    
+    // Add a new coffee type using spread operator and create an updated coffee type array
+    
+    console.log("Updated Coffee Types:", updatedCoffeeTypes);
+    
+    
